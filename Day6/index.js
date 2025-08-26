913be7fecc2eb2b299e9b9bf9edcd57c844b871d
Anand7879 <Anand7879@users.noreply.github.com>
2025-08-26 14:30:34 +0530

    Day6

---
+++ Day6/index.js
@@ -0,0 +1,70 @@
+// console.log("Anand Patel")
+
+// var a= 2.555
+// console.log(a)
+
+// Number => 5.5.66333,784543   We can assign any type of number
+// String => 'c' "cccc"
+
+// undefined
+// null
+// Boolean
+// BigInt
+// Symbol
+
+// var a
+// console.log(a);
+
+
+// var vs let vs const
+
+// var a = 5
+// var a = 10
+// console.log(a);
+
+// let a = 5
+// a = 20
+// console.log(a);
+
+
+// const a= 6
+// console.log(a);
+
+
+
+// console.log(5==5);
+// console.log(5=='5');// check only value not data type
+// console.log(5==='5'); //check  value and data type both
+// console.log(5==true);
+// console.log(5==false);
+// console.log(5==true);
+// console.log(5==' ');
+// console.log({}=={})
+
+
+// let a=100;
+// let to = a == 100;
+// console.log(to);
+
+// let userName = "Anand"
+// let lastName = "Patel"
+// console.log(userName+lastName);
+// console.log(userName,lastName);
+// console.log(userName+" "+lastName);
+// console.log(`${userName}  ${lastName}`)
+
+
+let a = true;
+a?console.log("Hii"):console.log("Byeee");
+
+if(a=true){
+    console.log("Hello");
+    
+}else{
+    console.log("hii");
+    
+}
+
+
+
+
