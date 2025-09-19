23fa7f88f21cc26b884a99b9d86c31d3b85b4e59
Anand7879 <Anand7879@users.noreply.github.com>
2025-09-19 11:15:14 +0530

    Day22

--- Day22/script.js
+++ Day22/script.js
@@ -41,50 +41,58 @@
 
 // console.log(maxSum);
 
-let arr = [1,2,-3,4,5];
-let maxSum = 0;
-let currSum = 0;
+// let arr = [1,2,-3,4,5];
+// let maxSum = 0;
+// let currSum = 0;
 
-for(let i  = 0; i<arr.length; i++){
-    currSum+=arr[i];
-    if(currSum>maxSum){
-        maxSum = currSum
-    }
-    if(currSum<maxSum){
-        currSum=0;
-    }
+// for(let i  = 0; i<arr.length; i++){
+//     currSum+=arr[i];
+//     if(currSum>maxSum){
+//         maxSum = currSum
+//     }
+//     if(currSum<maxSum){
+//         currSum=0;
+//     }
 
-}
+// }
 
 
 
-let p1 = new Promise((res,rej)=>{
-    res()
-})
+// let p1 = new Promise((res,rej)=>{
+//     res()
+// })
 
-let p2 = new Promise((res,rej)=>{
-    res()
-})
+// let p2 = new Promise((res,rej)=>{
+//     res()
+// })
 
-let p3 = new Promise((res,rej)=>{
-    res()
-})
+// let p3 = new Promise((res,rej)=>{
+//     res()
+// })
 
-Promise.all([p1,p2,p3]).then((res)=>{
-    console.log(res);
+// Promise.all([p1,p2,p3]).then((res)=>{
+//     console.log(res);
     
-})
+// })
 
-Promise.race([p1,p2,p3]).then((res)=>{
-    console.log(res);
+// Promise.race([p1,p2,p3]).then((res)=>{
+//     console.log(res);
     
-})
+// })
 
-Promise.allSettled([p1,p2,p3]).then((res)=>{
-    console.log(res);
+// Promise.allSettled([p1,p2,p3]).then((res)=>{
+//     console.log(res);
     
-})
+// })
 
+
+
+import user from "./home.js";
+console.log(user);
+
+import { lastName,sum} from "./home.js";
+console.log(lastName);
+sum();
 
 
 
