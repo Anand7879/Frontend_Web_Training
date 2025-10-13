06ca67dfb46411dc6f8fdcd7e428d5b1b1378980
Anand7879 <Anand7879@users.noreply.github.com>
2025-10-13 11:28:48 +0530

    Day 32 React

--- Day32/FirstReactApp9/src/App.jsx
+++ Day32/FirstReactApp9/src/App.jsx
@@ -1,72 +1,19 @@
-// import React from 'react'
-// import { useReducer } from 'react'
-
-// const App = () => {
-//   function reducer(count,action){
-//     if(action.type==='inc'){
-//       return count+1
-//     }
-//     else{
-//       return count
-//     }
-//   }
-
-//   let[count,dispatch] = useReducer(reducer,0)
-//   return (
-//     <div>
-//        <h2>{count}</h2>
-//        <button onClick={()=>dispatch({type:"inc"})}>++</button>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-// import React from 'react'
-// import { useReducer } from 'react'
-
-// const App = () => {
-//   function reducer(color,action){
-//     if(action.type==='r'){
-//       return 'red'
-//     }
-//     else if(action.type==='g'){
-//       return 'green'
-//     }     
-//     else if(action.type==='b'){
-//       return 'blue'
-//     }
-//     else{
-//       return color
-//     } 
-//   }
-
-//   let[color,dispatch] = useReducer(reducer,'white')
-
-//   return (
-//     <div style={{backgroundColor:color,height:'100vh'}}>
-//       <button onClick={()=>dispatch({type:'r'})}>Red</button>
-//       <button onClick={()=>dispatch({type:'g'})}>Green</button>
-//       <button onClick={()=>dispatch({type:'b'})}>Blue</button>
-
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-import React from 'react'
+import React ,{useState}from 'react'
 import Home from './Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  
-  let user="hello Anand"
+  let [apiData,SetData]=     useState([])
+  let [filteredddData,SetFilteredddData]=     useState([])
 
+    let [cart,SetCart]=    useState([])
+    
   return (
     <div>
-      <Home data={user}></Home>
+      <Routes>
+        <Route   path='/'  element={<Home cart={cart} SetCart={SetCart}   apiData={apiData} SetData={SetData} filteredddData={filteredddData}    SetFilteredddData={SetFilteredddData}  />} />
+        <Route   path='/cart'  element={<cart cart={cart} SetCart={SetCart} />} />
+      </Routes>
     </div>
   )
 }
---
+++ Day32/FirstReactApp9/src/cart.jsx
@@ -0,0 +1,50 @@
+// import React from 'react'
+
+// const cart = ({cart}) => {
+//   return (
+//     <div>
+//         {
+//             cart.map((a)=>{
+//                 return (<>
+//                 <div id='card'> 
+//                     <img  src={a.image}/>
+//             <p>{a.name}</p>
+//             <p> Rating: {a.rating}</p>
+//          <button  onClick={()=> SetCart([...cart,a])}>add </button>
+//          </div>
+//                 </>)
+
+//             })
+//         }
+//     </div>
+//   )
+// }
+
+// export default cart
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+const cart = ({cart}) => {
+    return (
+        <div>
+            <h1>Cart Items</h1>
+            {
+                cart.map((a) => {
+                    return (<>
+                        <div id='card'>
+                            <img src={a.image} />
+                            <p>{a.name}</p>
+                            <p> Rating: {a.rating}</p>
+                            <button onClick={() => SetCart([...cart, a])}>Add to Cart </button>
+                        </div>
+                    </>)    
+                })
+            }
+            <Link to={'/'}>
+                <button>Go to Home</button>
+            </Link>
+        </div>
+    )
+}
+
+export default cart
