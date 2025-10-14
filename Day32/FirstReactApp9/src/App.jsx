af82c046ac8a2fe5d950500aa3429ca55eba7312
Anand7879 <Anand7879@users.noreply.github.com>
2025-10-14 08:14:19 +0530

    Day 32 React

--- Day32/FirstReactApp9/src/App.jsx
+++ Day32/FirstReactApp9/src/App.jsx
@@ -1,22 +1,23 @@
 import React ,{useState}from 'react'
 import Home from './Home'
+import Cart from './Cart'
+
 import { Route, Routes } from 'react-router-dom'
+
 
 const App = () => {
   let [apiData,SetData]=     useState([])
   let [filteredddData,SetFilteredddData]=     useState([])
-
-    let [cart,SetCart]=    useState([])
+  let [cart,SetCart]=    useState([])
     
   return (
     <div>
       <Routes>
-        <Route   path='/'  element={<Home cart={cart} SetCart={SetCart}   apiData={apiData} SetData={SetData} filteredddData={filteredddData}    SetFilteredddData={SetFilteredddData}  />} />
-        <Route   path='/cart'  element={<cart cart={cart} SetCart={SetCart} />} />
+        <Route   path='/'  element={<Home cart={cart} SetCart={SetCart}   apiData={apiData} SetData={SetData} filteredddData={filteredddData}    SetFilteredddData={SetFilteredddData} ></Home>} />
+        <Route   path='/cart'  element={<Cart cart={cart} SetCart={SetCart} ></Cart>} />
       </Routes>
     </div>
   )
 }
 
 export default App
-
---
+++ Day33/FirstReactApp9/src/App.jsx
@@ -0,0 +1,21 @@
+import React ,{useState}from 'react'
+import Home from './Home'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  let [apiData,SetData]=     useState([])
+  let [filteredddData,SetFilteredddData]=     useState([])
+
+    let [cart,SetCart]=    useState([])
+    
+  return (
+    <div>
+      <Routes>
+        <Route   path='/'  element={<Home cart={cart} SetCart={SetCart}   apiData={apiData} SetData={SetData} filteredddData={filteredddData}    SetFilteredddData={SetFilteredddData}  />} />
+        <Route   path='/cart'  element={<cart cart={cart} SetCart={SetCart} />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
