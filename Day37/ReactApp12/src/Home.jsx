1079c5fb7a6850cf3b9c764b74daf59b743224db
Anand7879 <Anand7879@users.noreply.github.com>
2025-10-30 11:22:00 +0530

    navbar

--- Day37/ReactApp12/src/Home.jsx
+++ Day37/ReactApp12/src/Home.jsx
@@ -1,43 +1,8 @@
-import React, { useContext } from 'react'
-import Context from './Context'
-import './App.css'
-import { Link, useNavigate } from 'react-router-dom'
-
+import React from 'react'
 
 const Home = () => {
-    
-  let{state,dispatch}=useContext(Context)
-
- let navigate = useNavigate()
- function newPage(){
-      navigate(`/detail/${id}`)
- }
   return (
-    <>
-    <div>
-     <h1>🍽️ Recipe Menu</h1> 
-     <Link to="/cart">
-    <button id = 'cart'> 🛒Cart</button>
-     </Link>
-        <div>
-        {
-          state.apiData.map((a)=>{
-            return(
-              <div id='one' onClick={newPage}>
-                    <img src={a.image}/>
-                    <p>{a.name}</p>
-                    <p id='p1'>⭐ {a.rating}</p>
-                    <button  onClick={()=>{
-                      dispatch({type:"addToCart",payload:a})
-                    }}>Add To Cart</button>
-                </div>
-                )
-              })
-          }
-        </div>
-    </div>
-    </>
-
+    <div>Home</div>
   )
 }
 
