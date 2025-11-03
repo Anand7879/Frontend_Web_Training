fafb5bc4e2dd21dc851c11a9c28ed2bda7749766
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-03 15:38:42 +0530

    useMemo & useRef

--- Day39/ReactApp16/src/App.jsx
+++ Day39/ReactApp16/src/App.jsx
@@ -21,25 +21,69 @@
 
 
 
-import React, { useState } from 'react'
-import { useMemo } from 'react'
+// import React, { useState } from 'react'
+// import { useMemo } from 'react'
+
+// const App = () => {
+
+//   let[data,SetData] = useState(0)
+//   let res =  useMemo(() => {
+//        let total = 0
+//         for(let i = 0; i<100000000; i++)
+//         {
+//             total+=i;
+//         }
+//         return total
+//       }, []);
+//       return (
+//         <div>
+//         <h2>{res}</h2>
+//         <h2>{data}</h2>
+//         <button onClick={()=>SetData(data+1)}>click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+//Problem with useEffect
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+//   let[input,SetInput] = useState("")
+//   let [data,SetData] = useState()
+
+//   useEffect(() => {
+//     SetData(data+1)
+//   })
+
+//   return (
+//     <div>
+//       <h1>{data}</h1>
+//       <input type="text" onChange={(e)=>SetInput(e.target.value)} />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+//solution
+import React, { use, useEffect, useRef, useState } from 'react'
 
 const App = () => {
+  let [input,SetInput] = useState("")
+  let count = useRef(0)
 
-  let[data,SetData] = useState(0)
-  let res =  useMemo(() => {
-       let total = 0
-        for(let i = 0; i<100000000; i++)
-        {
-            total+=i;
-        }
-        return total
-      }, []);
-      return (
-        <div>
-        <h2>{res}</h2>
-        <h2>{data}</h2>
-        <button onClick={()=>SetData(data+1)}>click</button>
+  useEffect(() => {
+    count.current = count.current + 1
+  })
+  return (
+    <div>
+   <h1>{count.current}</h1>
+      <input type="text" onChange={(e)=>SetInput(e.target.value)} />
     </div>
   )
 }
